6b17db123dc3df571512d4bdea7ba22bbf01bc83
chelseawei <chelsea33257@gmail.com>
2022-02-05 21:05:53 -0500

    added test function

--- src/com/company/qcz/test.java
+++ src/com/company/qcz/test.java
@@ -2,6 +2,13 @@ package com.company.qcz;
 
 public class test {
     public void test1(){
-        System.out.println("dfsgdg");
+        System.out.println("fsfhkafewhjfs");
     }
+    public void test5(){
+        System.out.println("fsfhkafewhjfs");
+    }
+    public void test6(){
+        System.out.println("fsfhkafewhjfs");
+    }
+
 }
